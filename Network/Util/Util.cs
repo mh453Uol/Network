453f9547b84e76f9cede93ab0b4f619b88c2cc1a
mh453Uol <mh453@student.le.ac.uk>
2020-12-31 11:59:11 +0000

    Hide like button if user not logged in.

---
+++ Network/Util/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Network.Util
+{
+    public static class Util
+    {
+        public static Nullable<Guid> GetUserId(this ClaimsPrincipal claims)
+        {
+            if (claims.Identity.IsAuthenticated)
+            {
+                return Guid.Parse(claims.FindFirstValue(ClaimTypes.NameIdentifier));
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
