e9503294f9f646ef4521eeed8961447bbf415ada
mh453Uol <mh453@student.le.ac.uk>
2020-12-31 09:07:40 +0000

    builing profile page

--- Network/Pages/Profile.cshtml.cs
+++ Network/Pages/Profile.cshtml.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Network.Core;
+using Network.Data;
 
 namespace Network.Pages
 {
@@ -14,22 +16,34 @@ namespace Network.Pages
     public class ProfileModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly NetworkDbContext _dbContext;
         private readonly ILogger<ProfileModel> _logger;
 
-        public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser ProfileUser { get; set; }
+        public bool UserLoggedIn { get; set; }
 
         public ProfileModel(ILogger<ProfileModel> logger,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            NetworkDbContext dbContext)
         {
             _userManager = userManager;
+            _dbContext = dbContext;
             _logger = logger;
         }
 
 
 
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(Guid id)
         {
-            ApplicationUser = await _userManager.GetUserAsync(User);
+            ProfileUser = await _dbContext.Users.Select(u => new ApplicationUser
+            {
+                Id = u.Id,
+                Firstname = u.Firstname,
+                Surname = u.Surname
+            })
+            .SingleOrDefaultAsync(u => u.Id == id);
+
+           UserLoggedIn = User.Identity.IsAuthenticated;
 
             return Page();
         }
